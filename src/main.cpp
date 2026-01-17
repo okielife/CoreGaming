9262e641b9b5391da8a590446bfb5b0a3aef5978
Edwin Lee <leeed2001@gmail.com>
2026-01-17 11:12:25 -0600

    Fix main signature

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "platform/platform.h"
 #include "game/game.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     Platform platform(800, 600, "SDL RPG");
     Game game(platform);
     platform.run([&](const float dt) {
